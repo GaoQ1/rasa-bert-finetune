87007a30da55df2fa70a6017f94602f0338bf34c
GaoQ1 <18912936976@163.com>
2019-01-11 14:28:39 +0800

    edit run.sh parameter

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ python run_classifier.py \
   --vocab_file $BERT_BASE_DIR/vocab.txt \
   --bert_config_file $BERT_BASE_DIR/bert_config.json \
   --init_checkpoint $BERT_BASE_DIR/bert_model.ckpt \
-  --max_seq_length 128 \
+  --max_seq_length 64 \
   --train_batch_size 32 \
   --learning_rate 3e-5 \
   --num_train_epochs 2.0 \
