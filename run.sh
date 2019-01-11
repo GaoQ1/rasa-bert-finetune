a110a73d8f97ada5cb3d34895e17a52eaecac3df
GaoQ1 <18912936976@163.com>
2019-01-11 13:43:51 +0800

    support rasa json bert finetune

---
+++ run.sh
@@ -0,0 +1,16 @@
+export BERT_BASE_DIR=./chinese_L-12_H-768_A-12
+export MY_DATASET=./data
+
+python run_classifier.py \
+  --task_name rasajson \
+  --do_train \
+  --do_eval \
+  --data_dir $MY_DATASET \
+  --vocab_file $BERT_BASE_DIR/vocab.txt \
+  --bert_config_file $BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint $BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length 128 \
+  --train_batch_size 2 \
+  --learning_rate 5e-5 \
+  --num_train_epochs 2.0 \
+  --output_dir ./rasa_model_output/
